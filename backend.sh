fd4fb46f0857a789a25c1aeeb245109a8fbbcee9
somashekar31098 <somashekar31098@gmail.com>
2024-05-05 13:14:44 +0530

    calling script

--- backend.sh
+++ backend.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-echo "please provide db password"
-read  mysql_root_password
+source ./common.sh
 
-Validate(){
-    if [ $1 -ne 0 ]
-  then 
-    echo -e "$2 is ....$R failure $N"
-    exit 1
- else
-    echo -e "$2 is ....$G sucess $N"
-    fi
-}
-
-if [ $USERID -ne  0 ]
-then 
-   echo "runn the script with root access"
-   exit 1
-else
-   echo "you are super user"
-fi
+check_root
 
 dnf module disable nodejs -y &>>LOGFILE
 Validate $? "disabling nodejs"
