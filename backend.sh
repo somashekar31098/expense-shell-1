c0c715ca8e2ec7e5399f377e5b4600ee5bd86374
somashekar31098 <somashekar31098@gmail.com>
2024-05-05 13:56:42 +0530

    calling script

--- backend.sh
+++ backend.sh
@@ -5,13 +5,13 @@ source ./common.sh  &>>LOGFILE
 check_root
 
 dnf module disable nodejs -y &>>LOGFILE
-Validate $? "disabling nodejs"
+#Validate $? "disabling nodejs"
 
 dnf module enable nodejs:20 -y &>>LOGFILE
-Validate $? "enabling nodejs"
+#Validate $? "enabling nodejs"
 
 dnf install nodejs -y &>>LOGFILE
-Validate $? "installing a nodejs"
+#Validate $? "installing a nodejs"
 
 id expense
 if [ $? -ne 0 ]
@@ -23,10 +23,10 @@ if [ $? -ne 0 ]
 fi
 
 mkdir -p /app &>>LOGFILE
-Validate $? "CREATING APP DIRECTORY"
+#Validate $? "CREATING APP DIRECTORY"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
-Validate $? "COPYING ZIP FILE"
+#Validate $? "COPYING ZIP FILE"
 
 cd /app 
 rm -rf /app/*
@@ -34,25 +34,25 @@ unzip /tmp/backend.zip &>>LOGFILE
 
 cd /app 
 npm install &>>LOGFILE
-Validate $? "installing nodejs dependencies"
+#Validate $? "installing nodejs dependencies"
 
-cp /home/ec2-user/expenses-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
-Validate $? "COPIED BACKEND SERVICE"
+cp /home/ec2-user/expenses-shell-1/backend.service /etc/systemd/system/backend.service &>>LOGFILE
+#Validate $? "COPIED BACKEND SERVICE"
 
 systemctl daemon-reload &>>LOGFILE
-Validate $? "system reload"
+#Validate $? "system reload"
 
 systemctl start backend &>>LOGFILE
-Validate $? "starting backend"
+#Validate $? "starting backend"
 
 systemctl enable backend &>>LOGFILE
-Validate $? "enabling backend"
+#Validate $? "enabling backend"
 
 dnf install mysql -y &>>LOGFILE
-Validate $? "installimg mysql client"
+#Validate $? "installimg mysql client"
 
 mysql -h db.daws1998.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>LOGFILE
-Validate $? "setting up schema"
+#Validate $? "setting up schema"
 
 systemctl restart backend &>>LOGFILE
-Validate $? "restating backend"
+#Validate $? "restating backend"
